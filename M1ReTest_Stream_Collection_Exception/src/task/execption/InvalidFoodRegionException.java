f3f8693a2f40ba536c08090d5fc5faec0d7960cc
HemalathaSubramani <145379711+HemalathaSubramani@users.noreply.github.com>
2023-10-13 12:20:28 +0530

    Add files via upload

---
+++ M1ReTest_Stream_Collection_Exception/src/task/execption/InvalidFoodRegionException.java
@@ -0,0 +1,9 @@
+package task.execption;
+
+public class InvalidFoodRegionException extends Exception{
+	
+	 public InvalidFoodRegionException(String message) {
+	        super(message);
+	    }
+
+}
